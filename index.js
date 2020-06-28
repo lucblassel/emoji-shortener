9608c7e0490d5552e5fa8a53338533ffe61b3da2
Luc Blassel <luc.blassel@gmail.com>
2020-06-28 02:33:09 +0200

    adding database and testing 📚

--- index.js
+++ index.js
@@ -4,18 +4,44 @@ const cors = require("cors");
 const monk = require("monk");
 const helmet = require("helmet");
 
-const app = express();
-const port = 3000
+// reading database address + secret
+require("dotenv").config();
 
-app.use(morgan('tiny')); // log requests
+const app = express();
+const port = 3000;
+
+const db = monk(process.env.MONGODB_URL);
+
+db.then(() => {
+  console.log("Monk connected to database 🔗");
+});
+
+const urls = db.get("urls");
+urls.createIndex({ emojis: 1 }, { unique: true });
+
+app.use(morgan("tiny")); // log requests
 app.use(cors()); // enable CORS
 app.use(helmet()); // secure app & set headers
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log(`request received: \n${req}`);
-    res.send('Hello There...');
+app.get("/", (req, res) => {
+  res.send("Hello There...");
+});
+
+app.get("/lasturl", (req, res) => {
+  urls.find({}).each((url, cursor) => {
+    res.send(JSON.stringify(url));
+    cursor.close();
+  });
+});
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id
+  urls.findOne({ emojis: id }).then(({ emojis, url }) => {
+    res.send(`These emojis: ${emojis} redirect to: ${url}`)
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port} 🦻`)
-})
+  console.log(`Listening on port ${port} 🦻`);
+});
